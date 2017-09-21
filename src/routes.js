228c3c842f3b4a6152a3c7d8a2d6b68dbc485ec7
soutionscience <marshnjagi@yahoo.com>
2017-09-21 15:31:56 +0300

    Update routes.js
    changed routes

--- src/routes.js
+++ src/routes.js
@@ -18,7 +18,7 @@
 		.state('categories',{
 			url:'/categories',
 			controller: 'categoriesCtrl as mainList',
-			templateUrl:'/templates/categories.template.html',
+			templateUrl:'templates/categories.template.html',
 			resolve: {
 				categories:['MenuDataService' , function(MenuDataService){
 					return MenuDataService.getAllCategories().then(function(result){
@@ -30,7 +30,7 @@
 		
                .state('items', {
                 url: '/items/{shortname}',
-                templateUrl: '/templates/items.template.html',
+                templateUrl: 'templates/items.template.html',
                 controller: 'itemsCtrl as  itemList',
                   resolve: {
                     itemsList: ['$stateParams', 'MenuDataService', function ($stateParams, MenuDataService) {
