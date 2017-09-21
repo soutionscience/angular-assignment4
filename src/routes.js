244bb0bbadf7155699c5032fdcb07125dea05df4
soutionscience <marshnjagi@yahoo.com>
2017-09-21 15:30:29 +0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -13,7 +13,7 @@
 		$stateProvider
 		.state('home', {
                 url: '/',
-                templateUrl: '/templates/home.template.html'
+                templateUrl: 'templates/home.template.html'
             })
 		.state('categories',{
 			url:'/categories',
